3c0211d8144099e5a11a9b5cff5fa8b5904b8301
rr <nikita.demchenko.rr@gmail.com>
2023-07-16 00:13:10 +0300

    fix

--- client/src/pages/Accounts.vue
+++ client/src/pages/Accounts.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
-import { Postcard, Plus, Delete, ChatDotSquare, Phone } from "@element-plus/icons-vue";
+import {
+  Postcard,
+  Plus,
+  Delete,
+  ChatDotSquare,
+  Phone,
+  Notebook
+} from "@element-plus/icons-vue";
 import { reactive, ref } from "vue";
 import { vMaska, type MaskaDetail } from "maska";
 import { displayPhone } from "~/utils/phone";
 import type { IAccount } from "~/interfaces/IAccount";
-const form = ref<{ phone: string }>({ phone: "" });
 const phoneMask = reactive<MaskaDetail>({
   masked: "",
   unmasked: "",
   completed: false,
 });
 
+const form = ref<{ phone: string }>({ phone: "" });
 const accounts = ref<IAccount[]>([]);
+const search = ref("");
 
 function addAccount() {
   accounts.value.push({
@@ -45,27 +53,44 @@ function addAccount() {
       </div>
     </el-form>
     <div class="accounts-list" v-if="accounts.length">
-      <h3>Список добвленных</h3>
+      <h3><el-icon><Notebook /></el-icon> Список добвленных</h3>
       <div class="row">
-        <div class="col-12 col-md-6 col-lg-4 mb-4" v-for="item in accounts">
-          <el-card>
-            <template #header>
-              <div class="d-flex justify-content-between">
-                <el-button size="small" :icon="ChatDotSquare" type="success"
-                  >Список чатов</el-button
-                >
-                <el-button
-                  size="small"
-                  type="warning"
-                  :icon="Delete"
-                ></el-button>
-              </div>
-            </template>
-            <div style="font-size: 14px">
-              <el-icon><Phone /></el-icon> {{ displayPhone(item.phone) }}
-            </div>
-          </el-card>
+        <div class="col-12 col-md-6 col-lg-4">
+          <el-input
+            v-maska
+            data-maska="+# (###) ### ## ##"
+            class="mb-4"
+            placeholder="Поиск..."
+            v-model="search"
+          >
+          </el-input>
         </div>
+      </div>
+      <div class="row">
+        <template v-for="item in accounts">
+          <div
+            class="col-12 col-md-6 col-lg-4 mb-4"
+            v-if="displayPhone(item.phone).includes(search)"
+          >
+            <el-card>
+              <template #header>
+                <div class="d-flex justify-content-between">
+                  <el-button size="small" :icon="ChatDotSquare" type="success">
+                    Список чатов
+                  </el-button>
+                  <el-button
+                    size="small"
+                    type="warning"
+                    :icon="Delete"
+                  ></el-button>
+                </div>
+              </template>
+              <div style="font-size: 14px">
+                <el-icon><Phone /></el-icon> {{ displayPhone(item.phone) }}
+              </div>
+            </el-card>
+          </div>
+        </template>
       </div>
     </div>
   </div>
